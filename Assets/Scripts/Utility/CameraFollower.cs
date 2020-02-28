acf6ca43c36b596586d08472ca006488de4cd096
PolsCommits <paul.rauca@outlook.com>
2020-02-28 18:15:16 +0000

    Add CameraFollower.cs

---
+++ Assets/Scripts/Utility/CameraFollower.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    // Distance to keep between camera and players
+    float followDistance = 2f;
+    // Obstacle detection range
+    public float avoidanceRange = 2f;
+    // Distance camera should move to avoid impact
+    public float avoidanceDistance = 0.2f;
+    // Speed at which camera should move to avoid impact
+    public float avoidanceSpeed = 1f;
+    // Player transforms to keep track of
+    public List<Transform> playerTransforms;
+
+    // Centre between all players
+    Vector3 idealPosition;
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void CalculateIdealPosition()
+    {
+        idealPosition = Vector3.zero;
+
+        for(int i = 0; i < playerTransforms.Count; i++)
+        {
+            idealPosition.x += playerTransforms[i].position.x;
+            idealPosition.z += playerTransforms[i].position.z;
+        }
+
+        idealPosition.x /= (float)playerTransforms.Count;
+        idealPosition.z /= (float)playerTransforms.Count;
+    }
+
+    // Returns how much the camera should zoom in(+) or out (-) in order to fit a transform in the view
+    float DistanceToMove(Transform t)
+    {
+        Vector3 pos = t.position;
+        Vector3 cameraPos = transform.position;
+        float distanceToMove = 0;
+        float angleBetween = Vector3.Angle(transform.forward, (pos - cameraPos).normalized);
+
+        int i = 0;
+        while(angleBetween > GetComponent<Camera>().fieldOfView / 2)
+        {
+            distanceToMove += 1f;
+            cameraPos -= transform.forward;
+
+            angleBetween = Vector3.Angle(transform.forward, (pos - cameraPos).normalized);
+            i++;
+            if(i == 1000)
+            {
+                Debug.LogError("Yikes");
+                break;
+            }
+        }
+
+        return distanceToMove;
+    }
+
+    float ZoomChange()
+    {
+        float zoom = 0f;
+
+        foreach(Transform t in playerTransforms)
+        {
+            if(DistanceToMove(t) > zoom)
+                zoom = DistanceToMove(t);
+        }
+
+        return zoom;
+    }
+    
+    void Update()
+    {
+        CalculateIdealPosition();
+
+        // Check if camera will impact anything
+        // if(Physics.Raycast(transform.position, transform.forward, avoidanceRange))
+        // {
+        //     idealPosition.x += Mathf.Sign(transform.position.x) * avoidanceDistance;
+        //     Debug.DrawRay(transform.position, transform.forward * avoidanceRange, Color.red);
+        // }
+        // else
+        // {
+        //     Debug.DrawRay(transform.position, transform.forward * avoidanceRange, Color.green);
+        // }
+
+        idealPosition.y = playerTransforms[0].position.y + followDistance + ZoomChange();
+
+        //transform.position = Vector3.Lerp(transform.position, idealPosition, Time.deltaTime);
+        rigidbody.MovePosition(Vector3.Lerp(transform.position, idealPosition, Time.deltaTime));
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        rigidbody.AddForce(transform.up);
+    }
+}
---
+++ Assets/Scripts/Utility/LevelManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Speed at which the level plane is falling in units per second
+    public float fallingSpeed = 5f;
+    // How much time the level should take in seconds
+    public float levelTimer = 360f;
+    // Length of a segment in Unity units
+    public float segmentSize = 2f;
+    // How many segments ahead should be rendered
+    public int visibleSegmentsAhead = 3;
+    // Transform of the level plane
+    public Transform level;
+    // Start/End prefabs
+    public GameObject startPrefab;
+    public GameObject endPrefab;
+    // Segment prefabs
+    public List<GameObject> segmentPrefabs;
+    GameObject[] segments;
+
+    // The number of segments of the level will be calculated at the start of the level, and will be progressively toggled on/off
+    int numberOfSegments;
+
+    void CalculateNumberOfSegments()
+    {
+        numberOfSegments = Mathf.RoundToInt(fallingSpeed * levelTimer / segmentSize);
+    }
+
+    void GenerateSegments()
+    {
+        CalculateNumberOfSegments();
+        segments = new GameObject[numberOfSegments];
+
+        segments[0] = Instantiate(startPrefab, new Vector3(0, -segmentSize, 0), Quaternion.identity, transform);
+        segments[numberOfSegments - 1] = Instantiate(endPrefab, new Vector3(0, -(numberOfSegments - 1) * segmentSize, 0), Quaternion.identity, transform);
+
+        for(int i = 1; i < numberOfSegments - 1; i++)
+        {
+            segments[i] = Instantiate(segmentPrefabs[Random.Range(0, segmentPrefabs.Count)], new Vector3(0, -segmentSize * i, 0), Quaternion.identity, transform);
+        }
+    }
+
+    void Start()
+    {
+        GenerateSegments();
+    }
+
+    void Update()
+    {
+
+    }
+}
