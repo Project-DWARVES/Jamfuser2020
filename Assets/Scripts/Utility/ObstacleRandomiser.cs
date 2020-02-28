4f0f13d5b0a7c1ef3b7b1ff5e8d98ee9f94b69c9
Joseph Perring <JP108651@falmouth.ac.uk>
2020-02-28 18:01:19 +0000

    #Added
    *Intro Scene text
    * Menu Manager Script
    * Victory Scene

--- Assets/Scripts/Utility/ObstacleRandomiser.cs
+++ Assets/Scripts/Utility/ObstacleRandomiser.cs
@@ -2,73 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class ObstacleLocation
-{
-    public Transform transform;
-    [HideInInspector] public GameObject obstacle;
-    [HideInInspector] public bool populated;
-}
-
 public class ObstacleRandomiser : MonoBehaviour
 {
-    // Min/Max rotation of objects
-    [Tooltip("Minimum/Maximum rotation of this object when spawned")] public Vector2 initialRotation = new Vector2Int(0, 0);
-
-    // Min/Max number of obstacles to spawn
-    [Tooltip("Minimum/Maximum number of obstacles to spawn")] public Vector2Int obstaclesAmount = new Vector2Int(0, 10);
-    // List of transforms where to place objects
-    public List<ObstacleLocation> obstacleLocations;
-    // List of obstacles to place
-    public List<GameObject> obstacles;
-
+    // Start is called before the first frame update
     void Start()
     {
-        // Mark all positions as not populated
-        foreach(ObstacleLocation o in obstacleLocations)
-            o.populated = false;
-
-        transform.Rotate(0, Random.Range(-1, 2) * Random.Range(initialRotation.x, initialRotation.y), 0);
-
-        Populate();
+        
     }
 
-    // Instantiate obstacle in location
-    void PlaceObstacle(ObstacleLocation location, GameObject obstacle)
+    // Update is called once per frame
+    void Update()
     {
-        location.obstacle = Instantiate(obstacle, location.transform.position, location.transform.rotation, location.transform);
-        location.populated = true;
-    }
-
-    // Place random obstacles
-    public void Populate()
-    {
-        if(obstacleLocations.Count > 0 && obstacles.Count > 0)
-        {
-            // Get a random number of obstacles to place
-            int amount = Random.Range(obstaclesAmount.x, obstaclesAmount.y + 1);
-            // Make sure there are enough obstacles
-            amount = Mathf.Min(amount, obstacleLocations.Count);
-
-            for(int i = 0; i < amount; i++)
-            {
-                bool placedObstacle = false;
-
-                while(!placedObstacle)
-                {
-                    int obstacle = Random.Range(0, obstacleLocations.Count);
-
-                    if(!obstacleLocations[obstacle].populated)
-                    {
-                        PlaceObstacle(obstacleLocations[obstacle], obstacles[Random.Range(0, obstacles.Count)]);
-                        placedObstacle = true;
-                    }
-                }
-            }
-        }
-        else
-        {
-            Debug.LogError("No obstacle locations/obstacles have been set for " + name);
-        }
+        
     }
 }
